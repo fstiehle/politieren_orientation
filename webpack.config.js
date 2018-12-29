c49a097ef5939f07a3288584d858eddf522a6720
Fabian Stiehle <f.stiehle@gmail.com>
2018-12-29 12:28:58 +1100

    sacfold project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+  context: __dirname + "/src",
+  entry: "./app",
+  output: {
+		path: __dirname + "/dist",
+		filename: "bundle.js"
+	},
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  }
+};
