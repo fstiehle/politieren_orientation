b5de24bc445dab3ed2f09d70b16bed4dba91b20a
Fabian Stiehle <f.stiehle@gmail.com>
2019-02-16 01:04:18 +1100

    update button texts

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Buttons = ({click}) => (
   <div className="test-buttons antworten">
-    <button className="test-button" value="2" onClick={click}>Stimme sehr zu</button>
-    <button className="test-button" value="1" onClick={click}>Stimme zu</button>
+    <button className="test-button" value="2" onClick={click}>Ja</button>
+    <button className="test-button" value="1" onClick={click}>Eher Ja</button>
     <button className="test-button" value="0" onClick={click}>Neutral</button>
-    <button className="test-button" value="-1" onClick={click}>Stimme nicht zu</button>
-    <button className="test-button" value="-2" onClick={click}>Stimme gar nicht zu</button>
+    <button className="test-button" value="-1" onClick={click}>Eher Nein</button>
+    <button className="test-button" value="-2" onClick={click}>Nein</button>
     <button className="test-button skip" value="_" onClick={click}>Keine Meinung</button>
   </div>
 )
